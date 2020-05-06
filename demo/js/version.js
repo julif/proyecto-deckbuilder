92089a279d1701a9beb80e518787dedd18934388
Julif <julif-@live.com>
2020-05-06 03:05:13 -0300

    v 0.2.25.1

--- demo/js/version.js
+++ demo/js/version.js
@@ -1,2 +1,31 @@
 //version
 $("div.version").html("v 0.2.25");
+
+
+
+
+
+function splash_img_loaded(){
+    loaded_slpash_img = true;
+    //console.log( " imagen del splash: "+loaded_slpash_img);
+    //setTimeout(fadediv, 2000, splash, splash_art);
+ }
+ var img_splash_counter = 0;
+ iniciar_splash();
+ function iniciar_splash() {
+   var myVar = setInterval(myTimer, 3000)
+   function myTimer() {
+     if (img_splash_counter>10) { 
+       // alert("la aplicacion esta tomando demasiado tiempo para cargar, actualizando la app");
+       // location.reload();
+       window.location.reload(false); 
+     }
+     if (loaded_slpash_img == true) { fadediv(splash, splash_art); clearInterval(myVar);}
+     else if(loaded_slpash_img == false){
+       //alert(" recargar ");
+       img_splash_counter++;
+       console.log("carga de img_splash fallido N:"+img_splash_counter);
+       iniciar_splash();
+     }
+   }
+ }//iniciar_splash
