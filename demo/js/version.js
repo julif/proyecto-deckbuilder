64243b373c1aba0e2cca5e6abf74639366547367
Julif <julif-@live.com>
2020-05-06 03:38:27 -0300

    v 0.2.25.4

--- demo/js/version.js
+++ demo/js/version.js
@@ -1,11 +1,11 @@
 //version
-$("div.version").html("v 0.2.25.3");
+$("div.version").html("v 0.2.25.4");
 
 
 // $("#precarga_img_splash-art").load(function(){
 //     setTimeout(fadediv, 2000, splash, splash_art);
 //   });
 
-  setTimeout(fadediv, 2000, splash, splash_art);
+ 
 
 
