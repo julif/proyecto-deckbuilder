a30f600d187350c588383ce057516855b7c8fbb8
Julif <julif-@live.com>
2022-03-03 17:34:51 -0300

    v 0.1
    reboot

--- v 0.1/v 0.1.3/js/fullscreenchange.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('fullscreenchange', (event) => {
-  if (document.fullscreenElement) {
-    console.log(`Element: ${contenedor} entered full-screen mode.`);
-    document.getElementById('full').style.display= "none";
-  } else {
-    console.log('Leaving full-screen mode.');
-    document.getElementById('full').style.display= "inherit";
-  }
-});
--- v 0.1/v 0.1/js/precarga_principal.js
+++
@@ -1,7 +0,0 @@
-/* precarga principal  */
-function precarga_principal(){
-   // al cargar todas las imagenes
-   $('#precargador_principal').imagesLoaded( function() { 
-     none_flex('precarga_principal','precarga_principal_terminada');
-    });
-}
